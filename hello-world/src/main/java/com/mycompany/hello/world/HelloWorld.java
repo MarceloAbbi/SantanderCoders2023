8808aec9dd464c9922bff248b6c3988fd05e6546
marcelo <marcelo.abbi@outlook.com>
2023-11-15 18:04:49 -0300

    arquivos

---
+++ hello-world/src/main/java/com/mycompany/hello/world/HelloWorld.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hello.world;
+
+/**
+ *
+ * @author marce
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        String nome = "Marcelo";
+        System.out.println("Olá, " + nome);
+        
+        int a = 3;
+        int b = 2;
+        
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+        
+        System.out.println(soma);
+        
+        System.out.println(subtracao);
+        
+        System.out.println(multiplicacao);
+        
+        System.out.println(divisao);
+    }
+}
